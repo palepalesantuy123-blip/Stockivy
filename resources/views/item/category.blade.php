731cd6dee641d8f7676cf0d4ebb12a3a06b66528
Pulpen-ctrl <bejoismail04@gmail.com>
2026-05-20 09:15:24 +0700

    keram

--- resources/views/item/category.blade.php
+++ resources/views/item/category.blade.php
@@ -1,3 +1,30 @@
 <x-master>
-Hope you're happy lemon
+<div class="flex flex-col w-full pb-10 mx-auto px-6 max-w-400">
+  <div class="mt-5 card bg-[#F8F9FB] text-black w-185 h-50 border-2 border-gray-300 rounded-2xl flex items-center justify-center p-6">
+<div class="flex gap-3 mb-5 mr-140">
+  <div class="bg-[#E5E9F0] p-1.5 rounded-lg shrink-0 flex items-center justify-center w-12 h-12 ml-3">
+    <img src="/sort.png" alt="box" class="w-6 h-6 object-contain">
+  </div>
+  <div class="flex flex-col mt-3 text-sm font-bold text-black">
+    <span>Sort item</span>
+    <span>Category</span>
+  </div>
+  </div>
+<div class="w-168 h-8 rounded-lg border border-gray-300 flex items-center px-3 gap-2Focus-within:ring-2 focus-within:ring-blue-400">
+  <img src="/search.png" alt="search_button" class="w-4 h-4 object-contain opacity-50">
+  <input
+    type="text"
+    placeholder="Search product"
+    class="ml-1 bg-transparent border-none outline-none text-xs text-gray-700 w-full h-full placeholder-gray-400"
+  />
+</div>
+<div class="w-70 mt-3 h-8 rounded-lg border border-gray-300 flex items-center px-3 gap-2Focus-within:ring-2 focus-within:ring-blue-400">
+  <img src="/search.png" alt="search_button" class="w-4 h-4 object-contain opacity-50">
+  <input
+    type="text"
+    placeholder="Search product"
+    class="ml-1 bg-transparent border-none outline-none text-xs text-gray-700 w-full h-full placeholder-gray-400"
+  />
+
+</div>
 </x-master>
